3df0ee78c1a660a33da290428597ae38f96fa809
Judson Stephenson <Jud@users.noreply.github.com>
2026-07-14 21:22:07 -0500

    operator 0.1.11

--- Casks/operator.rb
+++ Casks/operator.rb
@@ -1,6 +1,6 @@
 cask "operator" do
-  version "0.1.10"
-  sha256 "20d555a4aaf73c9d2af1d237f8c2569e5c1e8e9c1052d63e133e41e9c84f6375"
+  version "0.1.11"
+  sha256 "bf0bffda62f43649910d9412d0cd9a4aef6194522dd4a652187a8c61c1d277f1"
 
   url "https://github.com/Jud/operator-releases/releases/download/v#{version}/Operator-#{version}.zip"
   name "Operator"
